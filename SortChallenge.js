d4e8c62780faee8d30beaf87e1db827b08adf685
Ghataorhe <cssb-ghataorhe@calvisitor-10-105-168-22.calvisitor.1918.berkeley.edu>
2018-04-05 19:04:42 -0700

    insertion & sort algorithms

---
+++ SortChallenge.js
@@ -0,0 +1,15 @@
+//selection algorithm
+var array = [201, 39, 54, 461, 93, 377, 413, 176, 164, 317, 109, 113, 36, 235, 17, 60, 53, 474, 257, 48];
+
+var otherArray = [];
+
+
+
+
+for (let i = 0; i < array.length; i++) {
+    if (array){
+
+    }    
+}
+
+
