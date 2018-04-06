4fdcaf4828a0dba1d5610a408f1f1bc6aa5e907f
Ghataorhe <cssb-ghataorhe@calvisitor-10-105-168-22.calvisitor.1918.berkeley.edu>
2018-04-05 19:19:07 -0700

    it works

--- Insertion-Algorithm.js
+++ Insertion-Algorithm.js
@@ -3,13 +3,9 @@
 // Using the pseudo-code below and what you can research online, implement Insertion sort in JavaScript.
 
 // - start at the beginning of the array
-
 // - move into a loop
-
 // - compare only 2 values
-
 // - swap if left is more than right
-
 // - repeat until this current value is less than the right value OR the value is at the far right of the array
 
 // - pseudo code:
@@ -28,3 +24,21 @@
 
 // end for
 // * Then, test your algorithm with each of the commented out arrays of data. As a bonus, use Benchmark.js to time it in both cases.
+
+// array to sort
+var array = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+
+function insertionSort(array) {
+  for(var i = 0; i < array.length; i++) {
+    var temp = array[i];
+    var j = i - 1;
+    while (j >= 0 && array[j] > temp) {
+      array[j + 1] = array[j];
+      j--;
+    }
+    array[j + 1] = temp;
+  }
+  return array;
+}
+
+console.log(insertionSort(array)); // => [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
